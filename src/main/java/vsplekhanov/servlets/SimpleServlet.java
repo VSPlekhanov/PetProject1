b6a4ecd27da4890666ef42789884e4a2463ea5b1
viacheslav <viacheslav_plekhanov@epam.com>
2019-01-30 12:28:13 +0300

    add simple post form

--- src/main/java/vsplekhanov/servlets/SimpleServlet.java
+++ src/main/java/vsplekhanov/servlets/SimpleServlet.java
@@ -5,14 +5,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-        writer.println(String.format("<h1> %s <h1>", req.getHeader("User-Agent")));
+        var writer = resp.getWriter();
+        writer.println(String.format("<h1> %s <h1>", req.getParameter("name")));
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp);
     }
 }
